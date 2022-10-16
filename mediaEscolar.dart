13fe7c7b0d4bee671b1b9d90a7a4600e8e3fa52d
Lior <liorlandin33@gmail.com>
2022-10-15 21:59:30 -0300

    Criação de variável

---
+++ mediaEscolar.dart
@@ -0,0 +1,14 @@
+void main (){
+   var media = 11;
+  
+   if (media >=6 && media <=10){
+     print (" Aprovado");
+   }else if(media <= 5 && media >=0){
+     print (" Reprovado");
+   }else{
+     print (" Média inválida, insira um valor de 0 a 10.");
+   }
+  
+  
+  
+}
